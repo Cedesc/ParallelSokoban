081d46adb3bb636b4bd49478e62c09bef25f9662
Christian Dohmen <cdohme01@students.uni-mainz.de>
2020-02-04 15:30:03 +0100

    blablabla zufaellige level erstellen und so

--- sokobanSettings.py
+++ sokobanSettings.py
@@ -1,14 +1,25 @@
-import sokobanLevelBib
+import sokobanLevelBib as slb
 
 """ Spiel-Settings """
 FENSTERBREITE = 1300
 FENSTERHOEHE = 1000
 SCHWIERIGKEIT = -1000
 einFeldKI = 0
-zweiFeldKI = (3, 2)
+zweiFeldKI = (1, 2)
 
+# beispielLevel, einfachesLevel, anderesLevel, kleineLevel, erstelltesLevel
+LEVEL = slb.beispielLevel
+#LEVEL = slb.kleineLevel
+#LEVEL = slb.anderesLevel
+#LEVEL = slb.erstelltesLevel
 
-VORGEFERTIGTESLEVEL = sokobanLevelBib.beispielLevel
-
-LEVEL = VORGEFERTIGTESLEVEL
-
+# beispielLevel zeigen
+# Schritte zurueckgehen
+# Level zuruecksetzen
+# KI zeigen
+# KI-Feld-Zaehler erhoehen und KI zeigen
+# kleineLevel
+# KI-2-Felder
+# anderesLevel
+# KI-2-Felder
+# zufaellige Level
